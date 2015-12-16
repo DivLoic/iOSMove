1228833eab4f48b9bec5170b0d99961d1c4d5eb5
Loïc <ldivad@isep.fr>
2015-12-16 22:41:40 +0100

    Fix regression of the son format
    TODO test introspection function intro & map

--- iOSMove/Measure.swift
+++ iOSMove/Measure.swift
@@ -16,12 +16,18 @@ class Measure: Object{
     // TODO: Fill properties and mathods
     func ping(){}
     
-    static func intro(m: Measure, f: () -> ()){
+    static func intro(m: Measure, f: (label: String?, value: Any) -> ()){
         let mirror = Mirror(reflecting:m)
-        mirror.children.forEach { (label: String?, value: Any) -> () in
-        
+        mirror.children.forEach(f)
+    }
+    
+    static func map(m: Measure, f: (label: String?, value: Any) -> ()) -> List{
+        var mapping: [Dictionary<String,Any>] = []
+        let mirror = Mirror(reflecting:m)
+        for child in mirror.children.enumerate(){
+            mapping.append([child.element.label!: child.element.value])
         }
-        
+        return mapping
     }
     
     
