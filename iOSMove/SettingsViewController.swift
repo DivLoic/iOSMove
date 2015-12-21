2af6c96db8f60d61484953b69d86d89574775465
Loïc <ldivad@isep.fr>
2015-12-21 19:00:14 +0100

    Vue of setting page
    still need a UIScollView

---
+++ iOSMove/SettingsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SettingsViewController.swift
+//  iOSMove
+//
+//  Created by Loïc M. DIVAD on 20/12/2015.
+//  Copyright © 2015 Loïc M. DIVAD. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SettingsViewController: ViewController{
+    
+    @IBOutlet var cellCollection: [UIView]!
+    @IBOutlet weak var onOff: UISwitch!
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var hostInput: UITextField!
+    @IBOutlet weak var axisSegment: UISegmentedControl!
+    
+    @IBOutlet weak var frequency: UILabel!
+    @IBOutlet weak var resetButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.design()
+    }
+    
+    
+    func design(){
+        for cell in cellCollection{
+            cell.layer.borderColor = UIColor(red: 0.871, green: 0.871, blue: 0.871, alpha: 1.00).CGColor
+            cell.layer.borderWidth = 0.3;
+        }
+        onOff.onTintColor = UIColor(red: 0.514, green: 0.741, blue: 0.667, alpha: 1.00)
+    }
+}
