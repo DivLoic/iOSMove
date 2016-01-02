dc1f543706a2639bb50a00dee4d7f291c824865f
Xavier <xavier.d971@gmail.com>
2016-01-02 08:27:56 +0100

    Add number of record + details on the last

--- iOSMove/DashboardViewController.swift
+++ iOSMove/DashboardViewController.swift
@@ -1,29 +1,65 @@
-//
+ //
 //  DashboardViewController.swift
 //  iOSMove
 //
 //  Created by Loïc M. DIVAD on 26/12/2015.
 //  Copyright © 2015 Loïc M. DIVAD. All rights reserved.
 //
+//import IOKit
 import RealmSwift
 import Foundation
 import Charts
 
 class DashboardViewController : ViewController{
 
-
-
+    // -- IBOutlet
+    
+    @IBOutlet weak var numberOfRecords: UILabel!
+    @IBOutlet weak var lastRec_x: UILabel!
+    @IBOutlet weak var lastRec_y: UILabel!
+    @IBOutlet weak var lastRec_z: UILabel!
+    @IBOutlet weak var lastRec_time: UILabel!
+    @IBOutlet weak var batteryLevel: UILabel!
     @IBOutlet weak var canvas: BubbleChartView!
+    
+    let backGround: UIColor = UIColor.whiteColor()
+    //var vcgreen =  UIColor(red: 0.518, green: 0.600, blue: 0.243, alpha: 1.00)
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let array1 = [5.0,0.5,4.0]
         let array2 = [2.0,3.5,6.0]
         let array3 = [8.0,5.5,10.0]
-        initChart()
-        bubleDraw(array1, y: array2, z : array3)
-    
+        //initChart()
+        //bubleDraw(array1, y: array2, z : array3)
+        showRecordsAmount()
+        showLastRecord()
+        showBatteryLevel()
     }
     
+    
+    func showRecordsAmount() {
+        let selected = AppDelegate.realm.objects(Acceleration.self)
+        let recNum = selected.count
+        self.numberOfRecords.text = self.numberOfRecords.text! + " " + String(recNum)
+    }
+    
+    func showLastRecord() {
+        let records = db.last(Acceleration.self, num: 2)
+        records.forEach { (map: Object) -> () in
+            let acc = map as! Acceleration
+            self.lastRec_x.text = "x : " + String(acc.x)
+            self.lastRec_y.text = "y : " + String(acc.y)
+            self.lastRec_z.text = "z : " + String(acc.z)
+            self.lastRec_time.text = self.lastRec_time.text! + " " + String(acc.datetime) + " - " + String(acc.time)
+        }
+    }
+    
+    func showBatteryLevel() {
+        //self.batteryLevel.double = IOPCopyPowerSourcesInfo()
+    }
+
     
     func bubleDraw(x:[Double], y:[Double], z:[Double]) {
         
@@ -65,7 +101,6 @@ class DashboardViewController : ViewController{
         canvas.getAxis(ChartYAxis.AxisDependency.Left).drawLabelsEnabled = false
 
     }
-    
 
     
     override func work() {
